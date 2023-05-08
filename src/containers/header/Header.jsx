2db8e0d1fe826efa86d87b35589a103c3fce51a0
abdulbasitali02 <103627220+abdulbasitali02@users.noreply.github.com>
2023-05-08 14:15:42 -0400

    Moved assets folder from components to src

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './header.css';
-
+import people from '../../assets/people.png';
+  
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="gpt4__header section__padding" id="home">
+      <div className="gpt4__header-content">
+        <h1 className="gradient__text">
+          Let's build something amazing with GPT-4 OpenAI
+        </h1>
+        <p>
+          ENTER THE PARAGRAPH TEXT IN THIS SPACE HERE
+        </p>
+
+        <div className="gpt4__header-content__input">
+          <input type="email " placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt4__header-content__people">
+          <img src={people}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
